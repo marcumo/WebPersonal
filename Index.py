edf2e7a482f3add68249db53f14b8da3eb5c0cd3
marcumo <31908883+marcumo@users.noreply.github.com>
2019-11-04 20:23:53 -0500

    agregado contenido

--- Index.py
+++ Index.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app    .route('/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('servicios.html')
 
 if __name__ == '__main__':
     app.run(debug= True)
